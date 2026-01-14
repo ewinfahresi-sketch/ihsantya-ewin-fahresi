09ca93e4ebd4eec121c46a0f25486e8e08a198bd
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-14 17:08:33 +0700

    Penambahan kode pada resi.routes.ts

--- api/src/routes/resi.route.ts
+++ api/src/routes/resi.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createResi } from "../controllers/resi.controller";
+import { createResi, getResiById } from "../controllers/resi.controller";
 
 const router = Router();
 
 router.post("/", createResi);
+router.get("/:id", getResiById);
 
 export default router;
