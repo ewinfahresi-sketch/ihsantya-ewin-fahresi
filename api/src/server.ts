4d7f284052241c599da0cfcff06a10dcb1abd376
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-14 13:03:47 +0700

    Menambahkan kode test sementara di server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,3 +3,10 @@ import app from "./app";
 app.listen(4000, () => {
   console.log("API running on http://localhost:4000");
 });
+
+import prisma from "./lib/prisma";
+
+app.get("/test-db", async (_req, res) => {
+  const count = await prisma.resi.count();
+  res.json({ message: "DB OK", totalResi: count });
+});
